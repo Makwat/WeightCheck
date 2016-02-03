564600114e0949918b77ef35f624e97b4593aeca
Makwat <makwat.fish@gmail.com>
2016-02-03 11:04:30 +0700

    Create table

--- app/src/main/java/kru/somwang/makwat/weightcheck/MyOpenHelper.java
+++ app/src/main/java/kru/somwang/makwat/weightcheck/MyOpenHelper.java
@@ -1,19 +1,32 @@
 package kru.somwang.makwat.weightcheck;
 
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by Administrator on 3/2/2559.
  */
-public class MyOpenHelper {
+public class MyOpenHelper extends SQLiteOpenHelper{
 
     public static final String database_name = "Weight.db"; //ประกาศค่าคงที่
-    private static  final  int database_Version = 1;
-    private static final String Create_table_weight = "create table weightTABLE (" +
+    private static  final  int database_version = 1;
+    private static final String create_table_weight = "create table weightTABLE (" +
             "_id integer primary key, " +
             "Date text, " +
             "weight text);";
 
     public MyOpenHelper(Context context) {
+        super(context, database_name, null, database_version);
     } //con
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(create_table_weight);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }// main class
