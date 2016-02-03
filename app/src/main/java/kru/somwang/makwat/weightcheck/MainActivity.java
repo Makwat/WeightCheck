aca34784df84aaf479d41bcf797c26f4e9132fc2
Makwat <makwat.fish@gmail.com>
2016-02-03 11:35:31 +0700

    Create SQLite

--- app/src/main/java/kru/somwang/makwat/weightcheck/MainActivity.java
+++ app/src/main/java/kru/somwang/makwat/weightcheck/MainActivity.java
@@ -5,9 +5,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MyManage objMyManage;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        objMyManage = new MyManage(this);
+
+    }// Main Method
+}// Main Class
