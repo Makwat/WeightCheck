6f4e2a4d7e701bf1dc884d0d8a6269d9d5b72cba
Makwat <makwat.fish@gmail.com>
2016-02-03 10:51:26 +0700

    Create Con fin

--- app/src/main/java/kru/somwang/makwat/weightcheck/MyOpenHelper.java
+++ app/src/main/java/kru/somwang/makwat/weightcheck/MyOpenHelper.java
@@ -1,9 +1,19 @@
 package kru.somwang.makwat.weightcheck;
 
+import android.content.Context;
+
 /**
  * Created by Administrator on 3/2/2559.
  */
 public class MyOpenHelper {
-    public MyOpenHelper() {
+
+    public static final String database_name = "Weight.db"; //ประกาศค่าคงที่
+    private static  final  int database_Version = 1;
+    private static final String Create_table_weight = "create table weightTABLE (" +
+            "_id integer primary key, " +
+            "Date text, " +
+            "weight text);";
+
+    public MyOpenHelper(Context context) {
     } //con
 }// main class
